67b9db62a886711917c753674410c4dd752b8d4d
chro11os <neilbragsg@gmail.com>
2025-03-30 11:34:02 +0800

    added background image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,19 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Tailwind CSS -->
     @vite('resources/css/app.css')
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 </head>
-<body class="bg-gray-100 text-gray-900">
-
-    <div class="container mx-auto p-4">
-        @yield('content')
+<body class="bg-cover bg-center bg-fixed" style="background-image: url('/img/background.png');">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="container p-7 m-7 backdrop-blur-md bg-gray-900 bg-opacity-50 rounded-lg">
+            <h1 class=""></h1>
+        </div>
     </div>
-
 </body>
 </html>
